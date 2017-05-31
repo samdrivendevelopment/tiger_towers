e84e6656405e28c34db7ae42a2d5fb8e11fd3080
Samallama <relentlessteaseip@gmail.com>
2017-05-30 23:01:29 -0700

    Second step done and tests. hard coded game works and tests run.

--- tiger_test.py
+++ tiger_test.py
@@ -1,5 +1,5 @@
 
-from tower_of_hanio import print_towers
+from tower_of_hanio import print_towers, move_disk
 
 class TestUI(object):
     def write(self, text):
@@ -14,8 +14,22 @@ def test_print_tower():
         [],
     ]
     print_towers(tower_list, ui)
-    return ui.written == 'O\nO21\nO'
+    return ui.written == 'O\nO21\nO\n\n'
 
+def test_move_disk():
+    tower_list = [
+        [],
+        ['2', '1'],
+        [], 
+    ]   
+    from_index = 1
+    to_index = 2
+    move_disk(tower_list, from_index, to_index)
+    return tower_list == [
+        [],
+        ['2'],
+        ['1'],
+    ]
 
 def main():
 
@@ -23,6 +37,9 @@ def main():
 
     if not test_print_tower():
         print 'Print towers did not print correctly.'
+
+    if not test_move_disk():
+        print 'Move disk did not move the disk properly.'
 
     print 'end test'
 
--- tower_of_hanio.py
+++ tower_of_hanio.py
@@ -7,16 +7,34 @@ def print_towers(tower_list, ui):
     first = 'O' + ''.join(tower_list[0])
     middle = 'O' + ''.join(tower_list[1])
     last = 'O' + ''.join(tower_list[2])
-    output = '\n'.join([first, middle, last])
+    output = '\n'.join([first, middle, last, '\n'])
     ui.write(output)
 
+def move_disk(tower_list, from_index, to_index):
+    disk = tower_list[from_index].pop()
+    tower_list[to_index].append(disk)
+
 def main():
-    ui = ShellUI()
     tower_list = [
+        ['3', '2', '1'],
         [],
-        ['2', '1'],
         [],
     ]
+    ui = ShellUI()
+    print_towers(tower_list, ui)
+    move_disk(tower_list, 0, 2)
+    print_towers(tower_list, ui)
+    move_disk(tower_list, 0, 1)
+    print_towers(tower_list, ui)
+    move_disk(tower_list, 2, 1)
+    print_towers(tower_list, ui)
+    move_disk(tower_list, 0, 2)
+    print_towers(tower_list, ui)
+    move_disk(tower_list, 1, 0)
+    print_towers(tower_list, ui)
+    move_disk(tower_list, 1, 2)
+    print_towers(tower_list, ui)
+    move_disk(tower_list, 0, 2)
     print_towers(tower_list, ui)
 
 if __name__ == '__main__':
